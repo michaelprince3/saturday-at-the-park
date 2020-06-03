c08e49362d9dc2ab962fb8a43fc4a2dd29e9c73c
michaelprince3 <michaelprince3@gmail.com>
2020-06-03 12:01:33 -0500

    test

--- src/components/itinerary/MyItinerary.js
+++ src/components/itinerary/MyItinerary.js
@@ -6,6 +6,7 @@ import useSimpleAuth from "../auth/useSimpleAuth"
 
 
 const MyItinerary = props => {
+    
     const [itineraryList, setItineraryList] = useState([])
     const { isAuthenticated } = useSimpleAuth()
 
